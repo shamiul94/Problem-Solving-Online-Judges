9f5f93bf259dba0eb16b544ba526ee55e5168c5f
shamiul94 <1505038.sh@ugrad.cse.buet.ac.bd>
2019-11-04 23:50:53 +0600

    triangle dp

--- LeetCode/120. Triangle.cpp
+++ LeetCode/120. Triangle.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 int dp[500][500] = {};
 
+
+// recursive solution - O(n^2) space
 class Solution {
 public:
     vector<vector<int>> arr;
@@ -31,6 +33,22 @@ public:
     }
 };
 
+// Iterative Solution - O(n) space
+class Solution2 {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<int> dp(triangle[n-1]);
+        for(int i = n-2 ; i >= 0 ; i--){
+            for(int j = 0 ; j <= i ; j++){
+                dp[j] = triangle[i][j] + min(dp[j] , dp[j+1]);
+            }
+        }
+        return dp[0];
+    }
+};
+
+
 int main() {
     vector<vector<int>> v
             {
@@ -49,3 +67,4 @@ int main() {
         cout << endl;
     }
 }
+
