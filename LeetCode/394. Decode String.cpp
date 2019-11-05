67e18ff122dfe8339b0cfe36f772c81c2604f9b5
shamiul94 <1505038.sh@ugrad.cse.buet.ac.bd>
2019-11-05 21:15:08 +0600

    decode string

--- LeetCode/394. Decode String.cpp
+++ LeetCode/394. Decode String.cpp
@@ -21,7 +21,7 @@ public:
     }
 
 
-    string solve(int l, int r) {
+    string solve1(int l, int r) {
         string ret = "";
 
         for (int i = l; i <= r;) {
@@ -50,7 +50,7 @@ public:
                     i++;
                 } while (uneven != 0);
                 newR = i - 2;
-                string nextLevelUnitString = solve(newL, newR);
+                string nextLevelUnitString = solve1(newL, newR);
                 for (int t = 0; t < newRepeat; t++) {
                     ret += nextLevelUnitString;
                 }
@@ -59,10 +59,77 @@ public:
         return ret;
     }
 
+
+    // memory optimized - by eliminating stream
+    string solve2(int l, int r) {
+        string ret = "";
+
+        for (int i = l; i <= r;) {
+            if (isAlpha(s[i])) {
+                ret += s[i];
+                i++;
+            } else if (isDigit(s[i])) {
+                int newRepeat = 0;
+
+                while (isDigit(s[i])) {
+                    newRepeat = 10 * newRepeat + (s[i] - '0');
+                    i++;
+                }
+                // now i == '['
+                int uneven = 0;
+                int newL, newR;
+                newL = i + 1;
+                do {
+                    if (s[i] == '[') {
+                        uneven++;
+                    } else if (s[i] == ']') {
+                        uneven--;
+                    }
+                    i++;
+                } while (uneven != 0);
+                newR = i - 2;
+                string nextLevelUnitString = solve2(newL, newR);
+                for (int t = 0; t < newRepeat; t++) {
+                    ret += nextLevelUnitString;
+                }
+            }
+        }
+        return ret;
+    }
+
+    // recursive but time efficient. 0 ms. (100%)
+    // This might be the most beautiful piece of recursive function I have seen in my entire life.
+    // This elegant piece of gold is inspired from here -
+    // https://leetcode.com/problems/decode-string/discuss/87544/Clean-C%2B%2B-Recursive-Solution-with-Explanation
+    string solve(int &idx) {
+        string ret = "";
+
+        int repeat = 0;
+        for (; idx < s.size(); idx++) {
+            if (isAlpha(s[idx])) {
+                ret += s[idx];
+            } else if (isDigit(s[idx])) {
+                repeat = 10 * repeat + (s[idx] - '0');
+            } else if (s[idx] == '[') {
+                string get = solve(++idx);
+                for (int i = 0; i < repeat; i++) {
+                    ret += get;
+                }
+                repeat = 0;
+            } else if (s[idx] == ']') {
+                return ret;
+            }
+        }
+        return ret;
+    }
+
+
     string decodeString(string str) {
         s = str;
         int n = s.length();
-        return solve(0, n - 1);
+//        return solve(0, n - 1);
+        int idx = 0;
+        return solve(idx);
     }
 };
 
