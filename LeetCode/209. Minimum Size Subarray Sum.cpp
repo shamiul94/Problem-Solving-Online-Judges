d69216d04b8d1ebe6b102bc64b97aa54161307dc
shamiul94 <1505038.sh@ugrad.cse.buet.ac.bd>
2019-11-12 20:32:35 +0600

    minimum sub-array sum.

--- LeetCode/209. Minimum Size Subarray Sum.cpp
+++ LeetCode/209. Minimum Size Subarray Sum.cpp
@@ -1,18 +1,107 @@
-class Solution {
+// my O(nlogn) solution
+// 66%, 90%
+
+
+class Solution1 {
 public:
     int minSubArrayLen(int s, vector<int> &nums) {
-        sort(nums.begin(), nums.end());
+        if (nums.size() == 0) return 0;
+
+        if (nums[0] >= s) return 1; // it's just an extended case of the below for loop.
+
         int n = nums.size();
-        int count = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            if (nums[i] <= s) {
-                s = s - nums[i];
-                count++;
-                if (s <= 0) {
-                    break;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] >= s) return 1;
+            nums[i] = nums[i - 1] + nums[i];
+        }
+
+        if (nums[n - 1] < s) return 0;
+        if (nums[n - 1] == s) return n;
+
+        int lo, hi, mid;
+        int minLen = INT_MAX;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] >= s) {
+                lo = 0;
+                hi = i - 1;
+                while (lo <= hi) {
+                    mid = (lo + hi) / 2;
+                    if (nums[i] - nums[mid] >= s) {
+                        lo = mid + 1;
+                    } else if (nums[i] - nums[mid] < s) {
+                        hi = mid - 1;
+                    }
                 }
+                minLen = min(minLen, i - mid + 1); // r-l+1
             }
         }
-        return count;
+        return minLen;
+    }
+
+
+};
+
+
+///////////////////////////
+
+// my O(n) solution - two pointer
+// 97%, 20%
+
+class Solution2 {
+public:
+    int minSubArrayLen(int s, vector<int> &nums) {
+        if (nums.size() == 0) return 0;
+
+        if (nums[0] >= s) return 1; // it's just an extended case of the below for loop.
+
+
+
+        int n = nums.size();
+        for (int i = 1; i < n; i++) {
+            if (nums[i] >= s) return 1;
+            nums[i] = nums[i - 1] + nums[i];
+        }
+
+        if (nums[n - 1] < s) return 0;
+        if (nums[n - 1] == s) return n;
+
+        nums.insert(nums.begin(), 0);
+
+
+        int l = 0;
+        int minLen = INT_MAX;
+
+        for (int i = 1; i < nums.size(); i++) {
+            while (nums[i] - nums[l] >= s) {
+                minLen = min(minLen, i - l);
+                l++;
+            }
+        }
+        return (minLen == INT_MAX) ? 0 : minLen;
+    }
+
+};
+
+/////////////
+
+// their O(n) solution - two pointer more optimized.
+// 97%,100%
+
+class Solution3 {
+public:
+
+    int minSubArrayLen(int s, vector<int> &nums) {
+        int n = nums.size();
+        int ans = INT_MAX;
+        int left = 0;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            while (sum >= s) {
+                ans = min(ans, i + 1 - left);
+                sum -= nums[left++];
+            }
+        }
+        return (ans != INT_MAX) ? ans : 0;
     }
 };
--- Testing/testin2.cpp
+++ Testing/testin2.cpp
@@ -24,12 +24,15 @@ int maxserver(int servers[], int n, int k) {
 
 
 int main() {
-    int servers[] = {1, 12, 5, 111, 200, 1000, 10, 9, 12, 15};
-//    int n = sizeof(servers) / sizeof(servers[7]); # 3 === array index
-    int n = sizeof(servers) / sizeof(servers[8888] );
-    int k = 5000;
+//    int servers[] = {1, 12, 5, 111, 200, 1000, 10, 9, 12, 15};
+////    int n = sizeof(servers) / sizeof(servers[7]); # 3 === array index
+//    int n = sizeof(servers) / sizeof(servers[8888] );
+//    int k = 5000;
+//
+//    cout << maxserver(servers, n, k);
+////    cout << servers[8888];
+//    return 0;
 
-    cout << maxserver(servers, n, k);
-//    cout << servers[8888];
-    return 0;
+    int arr[1100][1100] = {};
+    cout << arr[100][100] << endl;
 }
