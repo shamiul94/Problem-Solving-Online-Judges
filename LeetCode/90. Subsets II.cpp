14861359c6c4968074ae3ed9a7a74512cb814bd5
shamiul94 <1505038.sh@ugrad.cse.buet.ac.bd>
2019-11-21 14:43:23 +0600

    subsets-2 optimized more.

--- LeetCode/90. Subsets II.cpp
+++ LeetCode/90. Subsets II.cpp
@@ -4,7 +4,7 @@
 // this type of problem requires backtracking.
 // permutation, combination, subset, subset sum - these require backtracking.
 
-class Solution {
+class Solution1 {
 public:
     void solve(vector <vector<int>> &vec, vector<int> &nums, vector<int> &v, int start) {
         vec.push_back(v);
@@ -33,3 +33,67 @@ public:
         return vec;
     }
 };
+
+
+//////////////////
+
+// 99%, 100%
+// This is also my original solution : got idea from a discussion post of subset-1
+// for that solution, look at subset-1 second solution.
+// that problem had no duplicate.
+// I made it work for duplicate.
+// I am proud of this solution.
+
+
+// prerequisite to understand this solution:
+// you need to review the second solution of 'Subsets' (https://leetcode.com/problems/subsets/) problem first.
+//
+// This solution is just an extension of that solution.
+
+// idea: the idea is that -
+// suppose you have {1,2,3,3,3}
+// Now serially, you get -
+// 1. null
+// 2. null | 1
+// 3. null | 1 | 2 ; 1,2
+// 4. null | 1 | 2 ; 1,2 | 3; 1,3; 2,3; 1,2,3
+// 5. null | 1 | 2 ; 1,2 | 3; 1,3; 2,3; 1,2,3 | 3,3; 1,3,3; 2,3,3; 1,2,3,3
+// 6. null | 1 | 2 ; 1,2 | 3; 1,3; 2,3; 1,2,3 | 3,3; 1,3,3; 2,3,3; 1,2,3,3 | 3,3,3; 1,3,3,3; 2,3,3,3; 1,2,3,3,3
+// You can see that, you don't need to add this number to all the previous subsets
+// if the new number is repeated.
+// you need to add this number to the last created subsets in the previous step.
+// so, you save the index number in 2-d vector of the first subset which you created in
+// the last iteration (which is == n, because the size of vec<vec> before that was n and last index was n-1 ).
+// prevLow is that index.
+
+
+class Solution {
+public:
+
+    vector <vector<int>> subsetsWithDup(vector<int> &nums) {
+        vector <vector<int>> vec;
+        sort(nums.begin(), nums.end());
+        vector<int> u;
+        vec.push_back(u);
+
+        int prevLow;
+        int start;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int n = vec.size();
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                start = prevLow;
+            } else {
+                start = 0;
+            }
+            for (int j = start; j < n; j++) {
+                vector<int> v = vec[j];
+                v.push_back(nums[i]);
+                vec.push_back(v);
+            }
+            prevLow = n;
+        }
+        return vec;
+    }
+};
+
