0e6aea262b9a1576f79050a9b881b66baf091363
shamiul94 <1505038.sh@ugrad.cse.buet.ac.bd>
2019-12-11 05:46:59 +0600

    fth

--- Online Judge Templates/LightOJ Template.cpp
+++ Online Judge Templates/LightOJ Template.cpp
@@ -100,6 +100,23 @@ ll setValueBit(ll n, ll pos, ll val) {
     return n;
 }
 
+inline int ReadInt() {
+    char c;
+    while (c = getchar(), c < '0' || c > '9');
+    int x = c - '0';
+    while (c = getchar(), c >= '0' && c <= '9')
+        x = x * 10 + c - '0';
+    return x;
+}
+
+inline void WriteInt(int x) {
+    int p = 1;
+    for (int temp = x / 10; temp > 0; temp /= 10)
+        p *= 10;
+    for (; p > 0; x %= p, p /= 10)
+        putchar(x / p + '0');
+}
+
 /************************************** END OF INITIALS ****************************************/
 
 int main() {
