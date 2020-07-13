7e8db6c11102fadb71c229b87f2f5411ee185c74
Shamiul Hasan <1505038.sh@ugrad.cse.buet.ac.bd>
2020-07-13 14:50:59 +0600

    LC-23

--- LeetCode/23. Merge k Sorted Lists.cpp
+++ LeetCode/23. Merge k Sorted Lists.cpp
@@ -11,18 +11,22 @@ struct ListNode {
 
 //////////////////// Map based - Time 67%, Memory 9% //////////////////////////
 
-/*
-Idea: 
-*/ 
+
+// Time: O(n*m), Space: O(u)
+
 class Solution {
 public:
     ListNode *mergeKLists(vector<ListNode *> &lists) {
+        // space: O(u): u = unique numbers
         unordered_map<int, int> um; // <unique number, count>
-        for (int i = 0; i < lists.size(); i++) {
-            ListNode *l = lists[i];
-            while (l) {
-                if (um.count(l->val) == 0) { // value is not in map
-                    um[l->val] = 1;
+
+        // O(n * m)
+        for (int i = 0; i < lists.size(); i++) { // O(n) : n = no of lists
+            ListNode *l = lists[i]; // O(1)
+            while (l) { // O(m) : m = average element number in a list
+                if (um.count(l->val) == 0) { // value is not in map 
+                // O(1) : hashmap's insertion, lookup time complexity is amortized constant.
+                    um[l->val] = 1; // O(1)
                 } else {
                     um[l->val]++;
                 }
@@ -31,19 +35,24 @@ public:
         }
 
         unordered_map<int, int>::iterator it;
+
+        // Space: O(u)
         vector<pair<int, int>> v;
 
+        // O(u) : u = unique element aong all lists. 
         for (it = um.begin(); it != um.end(); it++) {
             v.push_back(make_pair(it->first, it->second));
         }
 
+        // O(u*log(u))
         sort(v.begin(), v.end());
 
         ListNode *ans = new ListNode(0);
         ListNode *ret = ans;
 
-        for (int i = 0; i < v.size(); i++) {
-            for (int j = 0; j < v[i].second; j++) {
+        // O(u*avgOccur) == O(total element number) == O(n*m) ; (I argue)
+        for (int i = 0; i < v.size(); i++) { // O(u)
+            for (int j = 0; j < v[i].second; j++) { // O(avgOccur): avgOccur = average occurrence of a number 
                 ans->next = new ListNode(0);
                 ans = ans->next;
                 ans->val = v[i].first;
