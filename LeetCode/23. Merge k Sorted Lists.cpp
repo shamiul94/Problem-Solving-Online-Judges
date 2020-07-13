3d138f4ab00260920fda07f216477ff35c6ed93c
Shamiul Hasan <1505038.sh@ugrad.cse.buet.ac.bd>
2020-07-13 14:31:36 +0600

    LC-23

--- LeetCode/23. Merge k Sorted Lists.cpp
+++ LeetCode/23. Merge k Sorted Lists.cpp
@@ -1,12 +1,4 @@
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -16,63 +8,12 @@ struct ListNode {
     ListNode(int x) : val(x), next(NULL) {}
 };
 
-class Solution1 {
-public:
-    ListNode *mergeKLists(vector<ListNode *> &lists) {
-        int n = lists.size();
-        ListNode *l = new ListNode(0);
-        l->next = 0;
-        while (true) { // give some condition?
-            int minValThisRun = INT_MAX;
-            ListNode *minNode = 0;
 
-            for (int i = 0; i < n; i++) {
-                if (lists[i] != 0 && lists[i]->val < minValThisRun) {
-                    minValThisRun = lists[i]->val;
-                    minNode = lists[i];
-                }
-            }
-            if (minNode == 0) break;
-            l->next = minNode;
-            minNode = minNode->next;
-        }
-        return l->next;
-    }
-};
-
-/////////////////////////////////////////////////////////////
+//////////////////// Map based - Time 67%, Memory 9% //////////////////////////
 
 /*
-
-[
-  1->4->5,
-  1->3->4,
-  2->6
-]
-
-//O(m*n * log(mn))
-
-map[1] = 2  
-map[4] = 2  
-map[5] = 1  
-map[3] = 1  
-map[2] = 1  
-map[6] = 1  
-
-// O(u*log(u))
-
-vector<pair<int, int>> v {(1,2), (4,2), (5,1), (3,1), (2,1), (6,1)}; // O(u*log(u))
-
-After sorting: 
-
-passedTillNow = 0 
-idx = 0 
-
-vector<pair<int, int>> v {(1,2), (2,1), (3,1), (4,2), (5,1), (6,1)}; 
-
-
-*/
-
+Idea: 
+*/ 
 class Solution {
 public:
     ListNode *mergeKLists(vector<ListNode *> &lists) {
@@ -111,3 +52,33 @@ public:
         return ret = ret->next;
     }
 };
+
+
+///////////////// Bruteforce ///////////////////////
+
+class Solution1 {
+public:
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
+        int n = lists.size();
+        ListNode *l = new ListNode(0);
+        l->next = 0;
+        while (true) { // give some condition?
+            int minValThisRun = INT_MAX;
+            ListNode *minNode = 0;
+
+            for (int i = 0; i < n; i++) {
+                if (lists[i] != 0 && lists[i]->val < minValThisRun) {
+                    minValThisRun = lists[i]->val;
+                    minNode = lists[i];
+                }
+            }
+            if (minNode == 0) break;
+            l->next = minNode;
+            minNode = minNode->next;
+        }
+        return l->next;
+    }
+};
+
+/////////////////////////////////////////////////////////////
+
