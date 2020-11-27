606988784dfb077828b7a2627b836eeebb74f08c
shamiul94 <1505038.sh@ugrad.cse.buet.ac.bd>
2020-11-28 01:36:20 +0600

    LC-359

---
+++ LeetCode/359. Logger Rate Limiter.cpp
@@ -0,0 +1,35 @@
+class Logger {
+public:
+    unordered_map<string, int> um;
+    /** Initialize your data structure here. */
+    Logger() {
+    }
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    bool shouldPrintMessage(int timestamp, string message) {
+        int currTime = timestamp; 
+        
+        if(um.find(message) != um.end()){
+            // found 
+            int prevTime = um[message]; 
+            
+            if(currTime - prevTime < 10){ 
+                return false; 
+            } else {
+                um[message] = currTime;
+                return true; 
+            }
+        } else {
+            um[message] = currTime;
+            return true; 
+        }
+    }
+};
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger* obj = new Logger();
+ * bool param_1 = obj->shouldPrintMessage(timestamp,message);
+ */
