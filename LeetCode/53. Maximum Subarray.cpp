0d09d8c07d0191666f59c09c9cd6e992d3e2ec80
Shamiul Hasan <hasan.shamiul@bkash.com>
2022-08-05 16:23:16 +0600

    LC-53

--- LeetCode/53. Maximum Subarray.cpp
+++ LeetCode/53. Maximum Subarray.cpp
@@ -1,7 +1,23 @@
 class Solution
 {
 public:
-    int maxSubArray(vector<int> &nums)
+    
+    // i implemented this on my own without even rememberng/looking at kadane's algorithm
+    int maxSubArray(vector<int>& nums) {
+        
+        int currRunSum = -1000000; 
+        int maxSum = -1000000; 
+        
+        for(int i = 0; i < nums.size(); i++) {
+            currRunSum = max(currRunSum + nums[i], nums[i]); 
+            maxSum = max(maxSum, currRunSum);
+        }    
+        
+        return maxSum;
+    }
+
+
+    int maxSubArray2(vector<int> &nums)
     {
         int maxTotal = INT_MIN;
         int runningSum = 0;
